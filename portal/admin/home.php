5ba0bad8d6ad659fc6a79d8cf9cf9fd3b21ef2a4
CodeTerminator007 <Husnainahmad521@gmail.com>
2021-07-07 23:09:11 +0500

    notfication

--- portal/admin/home.php
+++ portal/admin/home.php
@@ -1,1 +1,27 @@
 <h1>Welcome to <?php echo $_settings->info('name') ?></h1>
+
+
+<?php
+
+										$conn = mysqli_connect("localhost", "root", "", "hussnainnasaruniversity");
+										 
+										// Check connection
+										if($conn === false){
+											die("ERROR: Could not connect. "
+												. mysqli_connect_error());
+										}
+										
+										$i =1;
+										$qry = $conn->query("SELECT * from admission_form");
+										$rows_count_value = mysqli_num_rows($qry);
+                                        
+										while($row=$qry->fetch_assoc()):
+																				
+										?>
+                                        <p> You have <?php echo $rows_count_value ?> admission forms</p>							
+
+<div class="alert alert-warning" role="alert">
+admission form from <?php echo $row['email'] ?>
+                                        </b></b></b><button type="button" class="btn btn-primary">check it</button>
+</div>
+<?php endwhile; ?>	
